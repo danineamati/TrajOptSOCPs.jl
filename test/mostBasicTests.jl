e66d1e08c2a6aa5605a3222713142748a20a02a0
Daniel Neamati <danineamati@gmail.com>
2020-08-12 15:28:42 -0700

    Moving to SafeTestsets framework

---
+++ test/mostBasicTests.jl
@@ -0,0 +1,11 @@
+# Simplest test, just to check the most basic functionality.
+
+using TrajOptSOCPs
+using Test
+
+@testset "Base Cases" begin
+    # Write your tests here.
+    @test fCheck2(4) == 16
+    @test fCheck2(8) == 64
+    @test fCheck2(10) == 100
+end
