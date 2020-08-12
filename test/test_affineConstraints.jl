910436b3c9b377aeccfe342aaa0c57806069820b
Daniel Neamati <danineamati@gmail.com>
2020-08-12 15:59:23 -0700

    Added affine inequality tests

--- test/test_affineConstraints.jl
+++ test/test_affineConstraints.jl
@@ -16,25 +16,26 @@ using Test
 end
 
 @testset "Affine Equality Constraint" begin
-    # Create a simple AL_AffineEquality object
+    # Create a higher dimensional AL_AffineEquality object
     dT2 = AL_AffineEquality([5 0; 0 6], [10; 12])
 
-    # Check that it can determine when it is on the line
+    # Check that it can determine when it is on the intersection
     @test TrajOptSOCPs.satisfied(dT2, [2; 2])
 
-    # And likewise when it is not on the line
-    ws = TrajOptSOCPs.whichSatisfied(dT2, [2; 4]) #(should return (True, False))
+    # And likewise when it is not on the intersection
+    ws = TrajOptSOCPs.whichSatisfied(dT2, [2; 4])
+    #(should return (True, False))
 
     @test ws[1]
     @test !ws[2]
 
     # Test Projections and Violations
     testpt = [10; 12]
-    # This point is not on the intersetion:
+    # This point is not on the intersection:
     @test !TrajOptSOCPs.satisfied(dT2, testpt)
 
     # Get the projections and norm
-    # pv = getProjVecs(dT2, testpt)
+    pv = TrajOptSOCPs.getProjVecs(dT2, testpt)
     gN = TrajOptSOCPs.getNormToProjVals(dT2, testpt)
 
     @test (gN'gN) != 0.0
@@ -49,31 +50,72 @@ end
 
 end
 
-    # println("\nAffine Inequality Constraints")
-    # diT = AL_AffineInequality([5], [10])
-    # println("** Simplest")
-    # println("On line    (True) : $(TrajOptSOCPs.satisfied(diT, 2))")
-    # println("Above line (False): $(satisfied(diT, 4))")
-    # println("Under line (True) : $(satisfied(diT, 0))")
-    # diT2 = AL_AffineInequality([5 0; 0 6], [10; 12])
-    # println("** 2D")
-    # println("On Intesection (True)        : $(satisfied(diT2, [2; 2]))")
-    # println("Check which not (True, False): $(whichSatisfied(diT2, [2; 4]))")
-    # println("Check which not (True, True) : $(whichSatisfied(diT2, [2; 0]))")
-    # println("** Testing the projections and violations")
-    # testpt = [10; 12]
-    # println("Point $testpt not at in region : $(!satisfied(diT2, testpt))")
-    # println("Projection onto feasible region : $(getProjVecs(diT2, testpt))")
-    # gN = getNormToProjVals(diT2, testpt)
-    # println("Violation is : $gN")
-    # println("Total Violation is : $(gN'gN)")
-    # testpt2 = [-10; -12]
-    # println("Point $testpt2 in feasible region : $(satisfied(diT2, testpt2))")
-    # println("Projection onto feasible region : $(getProjVecs(diT2, testpt2))")
-    # gN2 = getNormToProjVals(diT2, testpt2)
-    # println("Violation is : $gN2")
-    # println("Total Violation is : $(gN2'gN2)")
-    # println("** Testing Calculus")
-    # println("Hessian = $(getHessC(diT2))")
-    # println("Grad at $testpt = $(getGradC(diT2, testpt))")
-    # println("Grad at $testpt2 = $(getGradC(diT2, testpt2))")
+@testset "Affine Inequality Constraint Simple" begin
+    # Create a simple AL_AffineEquality object
+    diT = AL_AffineInequality([5], [10])
+
+    # Check that it can determine when it is on the line
+    @test TrajOptSOCPs.satisfied(diT, 2)
+
+    # And likewise when it is not on the line.
+    # Above the line (false)
+    @test !TrajOptSOCPs.satisfied(diT, 4)
+    # Under the line (true)
+    @test TrajOptSOCPs.satisfied(diT, 0)
+end
+
+@testset "Affine Inequality Constraint" begin
+    # Create a higher dimensional AL_AffineInequality object
+    diT2 = AL_AffineInequality([5 0; 0 6], [10; 12])
+
+    # Check that it can determine when it is on the intersection
+    @test TrajOptSOCPs.satisfied(diT2, [2; 2])
+
+    # And likewise when it is not on the intersection
+    ws = TrajOptSOCPs.whichSatisfied(diT2, [2; 4])
+    #(should return (True, False))
+
+    @test ws[1]
+    @test !ws[2]
+
+    # try a different set
+    ws = TrajOptSOCPs.whichSatisfied(diT2, [2; 0])
+
+    @test ws[1]
+    @test ws[2]
+
+    # Test Projections and Violations
+    testpt = [10; 12]
+    # This point is not on the intersection:
+    @test !TrajOptSOCPs.satisfied(diT2, testpt)
+
+    # Get the projections and violation
+    pv = TrajOptSOCPs.getProjVecs(diT2, testpt)
+    gN = TrajOptSOCPs.getNormToProjVals(diT2, testpt)
+
+    @test (gN'gN) != 0.0
+
+
+    # Try another test point
+    testpt2 = [-10; -12]
+
+    # This one should satisfy the constraint
+    @test TrajOptSOCPs.satisfied(diT2, testpt2)
+
+    # Get the projections and violation
+    pv2 = TrajOptSOCPs.getProjVecs(diT2, testpt2)
+    gN2 = TrajOptSOCPs.getNormToProjVals(diT2, testpt2)
+
+    @test (gN2'gN2) == 0.0
+
+    # Test the Calculus
+    gradDT2 = TrajOptSOCPs.getGradC(diT2, testpt)
+    gradDT22 = TrajOptSOCPs.getGradC(diT2, testpt2)
+    hessDT2 = TrajOptSOCPs.getHessC(diT2)
+
+    # Here we simply check the size.
+    @test size(gradDT2, 1) == 2
+    @test size(gradDT22, 1) == 2
+    @test size(hessDT2) == (2, 2)
+
+end
